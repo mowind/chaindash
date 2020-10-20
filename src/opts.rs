096055d8f644578152c39749726a21246d7ba46a
mowind <wjinwen.1988@gmail.com>
2020-10-20 13:52:46 +0800

    add interval option

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,5 @@
 use clap::Clap;
+use num_rational::Ratio;
 
 #[derive(Clap)]
 #[clap(version = "1.0", author = "mowind <wjinwen.1988@gmail.com>")]
@@ -6,4 +7,8 @@ pub struct Opts {
     /// The platon connection endpoints, separated by `,`.
     #[clap(long, default_value = "http://127.0.0.1:6789")]
     pub url: String,
+
+    /// Render interval
+    #[clap(long, default_value = "1")]
+    pub interval: Ratio<u64>,
 }
